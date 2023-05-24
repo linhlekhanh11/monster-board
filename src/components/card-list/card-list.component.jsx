3b966b4bbe571475dcfb4c37863b362675c67cef
Linh K Le <lkl007@ucsd.edu>
2023-05-23 17:29:45 -0700

    removed unused components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,10 @@
-import { Component } from "react";
 import './card-list.styles.css';
 import Card from "../card/card.components";
 
 const CardList = ({ monsters }) => {
     
     return (
-    <div className="card-list">
+    <div className="card-list" key={monsters.id}>
         {monsters.map((monster) => {
             return(
                 <Card monster={monster}/>
