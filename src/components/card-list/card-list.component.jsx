9b0120dabd330308322507cd8c4cbd70c1ae4d50
Linh K Le <lkl007@ucsd.edu>
2023-05-23 17:56:06 -0700

    fix react-dom and fix indent

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,17 +1,13 @@
 import './card-list.styles.css';
 import Card from "../card/card.components";
 
-const CardList = ({ monsters }) => {
-    
-    return (
-    <div className="card-list" key={monsters.id}>
+const CardList = ({ monsters }) => (
+    <div className="card-list">
         {monsters.map((monster) => {
-            return(
-                <Card monster={monster}/>
-            )         
-            })}        
+            return <Card monster={monster}/>;
+        })}        
     </div>
-    );
-}
+);
+
 
 export default CardList;
